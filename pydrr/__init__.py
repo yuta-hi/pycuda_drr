8e8b731cae1a43d899f4b51db12ac981a29c687d
kumata_sep17 <hiasa.yuta.ht7@is.naist.jp>
2018-06-30 22:31:42 +0900

    add pydrr

---
+++ pydrr/__init__.py
@@ -0,0 +1,6 @@
+import pycuda.autoinit
+from pydrr.KernelManager import KernelManager
+from pydrr.VolumeContext import VolumeContext
+from pydrr.GeometryContext import GeometryContext
+from pydrr.Detector import Detector
+from pydrr.Projector import Projector
